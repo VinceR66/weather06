4e01a6bd379ba14a6bd11c9c76913f49233623cd
VinceR66 <vrook@me.com>
2023-04-24 14:10:20 -0500

    added cards and dates

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,10 @@
 console.log("hello")
 
 
-var city = 'boston'
+
+var city = "boston"
+
+
 var API_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=9e7196afba18b75635e3489a9e6a6b9e'
 
 
@@ -21,4 +24,8 @@ fetch(API_WEATHER_URL)
         console.error(error);
     });
 
-console.log(main[1]);
+
+
+
+
+
