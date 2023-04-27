aed2d48aa40e00a1dddde40bcae0e6feb8d65c90
VinceR66 <vrook@me.com>
2023-04-26 20:40:34 -0500

    update to containers

--- assets/script.js
+++ assets/script.js
@@ -26,18 +26,32 @@ fetch(API_WEATHER_URL)
 
 //function createWeatherCard(list) {
 //  var cardEl = document.createElement('article');
-//  cardEl.classList.add('card, m-3');
-//   cardEl.style.width = '18rem';
 //cardEl.setAttribute('class', 'card, m-3');
 //cardEl.setAttribute('style', 'width: 18rem');
+
+//  var bodyEl = document.createElement('div');
+//  bodyEl.setAttribute('class', 'card-body');
+
+//  var pEl = document.createElement('p');
+//  pEl.setAttribute('class', 'card)       ???
+//  pEl.innerHTML = dayjs().format('MMMM, D, YYYY');  ???
+
+
+
 //  var imgEl = document.createElement('img');
-//  imgEl.setAttribute('src'.list.)
+//  imgEl.setAttribute('src',list.);
+//  imgEl.setAttribute('alt', 'weather condition Icon');
+//  imgEl.setAttribute('class', '??');
 
+//  var bodyEl = document.createElement('div');
+//  bodyEl.setAttribute('class', 'card-body');
 
-
-
-///  return cardEl;
+///  return cardEl.append(imgEl);
 //}
+
+
+//  cardEl.classList.add('card, m-3'); could use instead of setAttribute
+//   cardEl.style.width = '18rem';   could use instead of setAttribute
 
 
 //can i put icon # in src string using a variable
