1fc1c38700a50f488af58d32c0ed96fd276a37ec
VinceR66 <vrook@me.com>
2023-04-29 17:20:41 -0500

    cards have correct dates

--- assets/script.js
+++ assets/script.js
@@ -18,15 +18,13 @@ function getWeather(city) {
             dump.textContent = JSON.stringify(data.list, null, 2);
             document.body.appendChild(dump);
             console.log(data.list[0].main.temp);
-            // delete this
+
             renderWeatherCard(data.list);
-            //
             renderHeaderCard(data.list);
         })
         .catch(function (error) {
             console.error(error);
         });
-
 };
 
 document.querySelector('form').addEventListener('submit', function (event) {
@@ -34,10 +32,10 @@ document.querySelector('form').addEventListener('submit', function (event) {
     getWeather(this.city.value);
 });
 
+
 function createHeaderCard(firstCard) {
     var cardEl = document.createElement('article');
     cardEl.setAttribute('class', 'card m-3');
-    //cardEl.setAttribute('style', 'width: 18rem');
 
     // var imgEl = document.createElement('img');
     // imgEl.setAttribute('src', "https://openweathermap.org/img/wn/" + firstCard.weather[0].icon + "@2x.png");
@@ -47,9 +45,10 @@ function createHeaderCard(firstCard) {
     var bodyEl = document.createElement('div');
     bodyEl.setAttribute('class', 'card-body');
 
-    var titleEl = document.createElement('h5');
+    var titleEl = document.createElement('h3');
     titleEl.setAttribute('class', 'card-title');
-    titleEl.textContent = 'today';
+    titleEl.innerHTML = dayjs().add(0, 'day').format('MMMM, D, YYYY');
+
 
     var tempEl = document.createElement('p');
     tempEl.setAttribute('class', 'card-text');
@@ -71,15 +70,13 @@ function createHeaderCard(firstCard) {
 
 };
 
-
-
 function renderHeaderCard(cardOne) {
     var outcomeEl = document.getElementById('header-results');
     var headerCard = createHeaderCard(cardOne[0]);
     outcomeEl.append(headerCard);
 }
 
-//this gets deleted
+
 function createWeatherCard(weather) {
     var cardEl = document.createElement('article');
     cardEl.setAttribute('class', 'card m-3');
@@ -93,9 +90,9 @@ function createWeatherCard(weather) {
     var bodyEl = document.createElement('div');
     bodyEl.setAttribute('class', 'card-body');
 
-    var titleEl = document.createElement('h5');
+    var titleEl = document.createElement('h3');
     titleEl.setAttribute('class', 'card-title');
-    titleEl.textContent = 'tomorrow';
+    titleEl.innerHTML = dayjs().add(((i + 1) / 8), 'day').format('MMMM, D, YYYY');
 
     var tempEl = document.createElement('p');
     tempEl.setAttribute('class', 'card-text');
@@ -109,19 +106,12 @@ function createWeatherCard(weather) {
     humidityEl.setAttribute('class', 'card-text');
     humidityEl.textContent = 'Humidity: ' + weather.main.humidity;
 
-
-
-    //var linkEl = document.createElement('a');
-    //linkEl.href = 'https://www.imdb.com/title/' + movie.imdbId;
-    //linkEl.setAttribute('target', 'imdb');
-    //linkEl.textContent = 'More info';
-
-
     bodyEl.append(titleEl, tempEl, windEl, humidityEl);
 
     cardEl.append(imgEl, bodyEl);
 
     return cardEl;
+
 };
 
 function renderWeatherCard(forecast) {
@@ -135,161 +125,3 @@ function renderWeatherCard(forecast) {
 
 
 
-
-//
-
-// these get added back
-//document.getElementById("dateDay3").innerHTML = dayjs().add(2, 'day').format('MMMM, D, YYYY');
-//document.getElementById("dateTomorrow").innerHTML = dayjs().add(1, 'day').format('MMMM, D, YYYY');
-//document.getElementById("dateDay4").innerHTML = dayjs().add(3, 'day').format('MMMM, D, YYYY');
-//document.getElementById("dateDay5").innerHTML = dayjs().add(4, 'day').format('MMMM, D, YYYY');
-//document.getElementById("dateDay6").innerHTML = dayjs().add(5, 'day').format('MMMM, D, YYYY');
-//document.getElementById("dateToday").innerHTML = dayjs().format('MMMM, D, YYYY');
-
-
-/*
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//document.getElementById('temp1')
-
-
-
-
-
-//function createWeatherCard(weather) {
-
-//var cardEl = document.createElement('article');
-//cardEl.setAttribute('class', 'card, m-3');
-
-//var listEl = document.createElement('ul');
-//listEl.setAttribute('class', 'list-group, list-group-flush, card1');
-
-//var temp1 = document.createElement('li');
-//temp1.setAttribute('class', 'list-group-item item1');
-//temp1.setAttribute.textcontent = 'data.list[0].main.temp';
-
-//listEl.append(temp1)
-
-    //};
-
-
-
-
-
-
-
-
-
-
-
-//  var cardEl = document.createElement('article');
-//cardEl.setAttribute('class', 'card, m-3');
-//cardEl.setAttribute('style', 'width: 12rem');
-
-//  var bodyEl = document.createElement('div');
-//  bodyEl.setAttribute('class', 'card-body');
-
-//  var pEl = document.createElement('p');
-//  pEl.setAttribute('class', 'card')       ???
-//  pEl.innerHTML = dayjs().format('MMMM, D, YYYY');  ???
-
-//  var imgEl = document.createElement('img');
-//  imgEl.setAttribute('src', data.list[0].);
-//  imgEl.setAttribute('alt', 'weather condition Icon');
-//  imgEl.setAttribute('class', '??');
-
-
-
-
-///  return cardEl.append(imgEl);
-//}
-
-// useful trick - use comsole log to see if inputs are what you want then replace console log with function
-
-//  cardEl.classList.add('card, m-3'); could use instead of setAttribute
-//   cardEl.style.width = '18rem';   could use instead of setAttribute
-
-
-//can i put icon # in src string using a variable
-
-//    aria-describedby="emailHelp"   is this needed in form id
-
-// d-flex flex-
-/*
-var temp1 = data.link[0].main.temp;
-var temp2 = data.link[8].main.temp;
-var temp3 = data.link[16].main.temp;
-var temp4 = data.link[24].main.temp;
-var temp5 = data.link[32].main.temp;
-var temp6 = data.link[40].main.temp;
-
-var wind1 = data.link[0].wind.speed;
-var wind2 = data.link[8].wind.speed;
-var wind3 = data.link[16].wind.speed;
-var wind4 = data.link[24].wind.speed;
-var wind5 = data.link[32].wind.speed;
-var wind6 = data.link[40].wind.speed;
-
-var humidity1 = data.link[0].main.humidity;
-var humidity2 = data.link[8].main.humidity;
-var humidity3 = data.link[16].main.humidity;
-var humidity4 = data.link[24].main.humidity;
-var humidity5 = data.link[32].main.humidity;
-var humidity6 = data.link[40].main.humidity;
-
-var icon1 = data.link[0].weather[0].icon;
-var icon2 = data.link[8].weather[0].icon;
-var icon3 = data.link[16].weather[0].icon;
-var icon4 = data.link[24].weather[0].icon;
-var icon5 = data.link[32].weather[0].icon;
-var icon6 = data.link[40].weather[0].icon;
-*/
